58dfaf9f9cce29b3008225161c327dd88f3c3fac
Sly Vindictus <coolboyhd2014@gmail.com>
2022-10-19 08:57:05 +0200

    K

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+string brukerNavn = "kalle";
+string passord = "qwerty";
+
+string brukerInput;
+string passordInput;
+
+Console.WriteLine("####brukerNavn####");
+brukerInput = Console.ReadLine();
+Console.WriteLine("####passord####");
+passordInput = Console.ReadLine();
+
+if (brukerInput == brukerNavn && passordInput == passord)
+{
+    //### Inlogget #####
+}
+else
+{
+    //### Feil #####
+}
